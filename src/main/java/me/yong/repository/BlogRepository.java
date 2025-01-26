15ac14f0f838c75bb5ed44cae3275056332f17e2
YongLeeCode <yonghwan311@gmail.com>
2025-01-26 22:04:56 +0900

    feat: article 레포지토리 구현

--- src/main/java/me/yong/repository/BlogRepository.java
+++ src/main/java/me/yong/repository/BlogRepository.java
@@ -1,4 +1,8 @@
 package me.yong.repository;
 
-public class BlogRepository {
+import me.yong.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+
 }
