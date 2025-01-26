a02b8344b8653a593cd4503eb4624d9b8da90451
YongLeeCode <yonghwan311@gmail.com>
2025-01-26 22:06:59 +0900

    feat: 블로그 테스트 구현

--- src/test/java/me/yong/controller/BlogApiControllerTest.java
+++ src/test/java/me/yong/controller/BlogApiControllerTest.java
@@ -1,4 +1,98 @@
-import static org.junit.jupiter.api.Assertions.*;
+package me.yong.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import me.yong.domain.Article;
+import me.yong.repository.BlogRepository;
+import me.yong.dto.AddArticleRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc // MockMVC 생성 및 자동 구성
 class BlogApiControllerTest {
-  
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+
+    @DisplayName("블로그 글 추가")
+    @Test
+    public void addArticle() throws Exception {
+        //given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest req = new AddArticleRequest(title, content);
+
+        final String requestBody = objectMapper.writeValueAsString(req);
+
+        //when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        //then
+        result.andExpect(status().isCreated());
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1);
+        assertThat(articles.get(0).getTitle()).isEqualTo(title);
+        assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
+
+    @DisplayName("모든 블로그 글 조회")
+    @Test
+    public void findAll() throws Exception {
+        //given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        //when
+        ResultActions result = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        result
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
+    }
+
 }
