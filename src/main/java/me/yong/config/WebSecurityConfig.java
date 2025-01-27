04bc6109cf932fedfcd40a0500592ac33ccb6b60
YongLeeCode <yonghwan311@gmail.com>
2025-01-27 19:00:30 +0900

    feat: jwt와 스프링 시큐리티의 config 구현

--- src/main/java/me/yong/config/WebSecurityConfig.java
+++ src/main/java/me/yong/config/WebSecurityConfig.java
@@ -1,4 +1,70 @@
-package me.yong;
+package me.yong.config;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+import lombok.RequiredArgsConstructor;
+import me.yong.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final UserDetailService userDetailService;
+
+    // 스프링 시큐리티 기능 비활성화
+    // 인증, 인가 서비스를 모든 페이지에서 사용하지 않기 때문
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    // 위에서 스프링 시큐리티를 비활성화 했기 때문에 어떤 곳에서 인증/인가 및 로그인, 로그아웃 등을 할지 설정한다.
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests(auth -> auth
+                .requestMatchers(
+                        new AntPathRequestMatcher("/login"),
+                        new AntPathRequestMatcher("/signup"),
+                        new AntPathRequestMatcher("/user")
+                ).permitAll()
+                .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/articles")
+                ).logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                ).csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+            BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/me/yong/config/jwt/JwtProperties.java
+++ src/main/java/me/yong/config/jwt/JwtProperties.java
@@ -1,4 +1,15 @@
-package me.yong.config;
+package me.yong.config.jwt;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt") //yml파일에서 issuer와 secretKey 값 매핑
 public class JwtProperties {
+    private String issuer;
+    private String secretKey;
 }
